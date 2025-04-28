2d77806999abebf6a0a184ffcd4497b6b1422c81
hrusikeshspikewell <hrusikesh@spikewell.com>
2025-04-28 11:10:44 +0530

    notification dismiss

---
+++ x/Models/Route.swift
@@ -0,0 +1,12 @@
+//
+//  Route.swift
+//  x
+//
+//  Created by Hrusikesh-Spikewell on 25/04/25.
+//
+
+import SwiftUI
+
+enum Route: Hashable {
+    case profile, premium, bookmark, list, spaces, monetisation, settings, dark, timelineSettings
+}
